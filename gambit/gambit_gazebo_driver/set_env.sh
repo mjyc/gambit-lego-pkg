3c0751eb8f6f4c899f8b562c0480f2831a20ce1b
Mike Chung <mjyc@cs.washington.edu>
2013-06-13 22:36:20 -0700

    adding all changes

--- gambit/gambit_gazebo_driver/set_env.sh
+++ gambit/gambit_gazebo_driver/set_env.sh
@@ -1,5 +1,5 @@
 # use below command to set environmental variables
 # . ./set_env.sh
 source /usr/share/gazebo/setup.sh
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}~/Projects/HIL/codes/ros/stacks/ros-pkg-nsl/trunk/gambit/gambit_gazebo_driver/models/:
-export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}~/Projects/HIL/codes/ros/stacks/ros-pkg-nsl/trunk/gambit/gambit_gazebo_driver/lib:
+export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}~/ros/gambit/gambit_gazebo_driver/models/:
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}~/ros/gambit/gambit_gazebo_driver/lib:
