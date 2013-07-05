a7139c16c4d132bbeb2c5c6350ac9327e264e28b
robotlab <robotlab@celilo.(none)>
2013-07-05 01:40:15 -0700

    default path changed

--- set_env.sh
+++ set_env.sh
@@ -4,4 +4,4 @@
 alias set_rosmasteruri_chessbot="export ROS_MASTER_URI=http://chess-laptop.dyn.cs.washington.edu:11311;set_ros_ip"
 alias set_rosmasteruri_chessarmbox="export ROS_MASTER_URI=http://chess-armbox.dyn.cs.washington.edu:11311;set_ros_ip"
 
-ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/ros/gambit-lego-pkg/
+ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/ros-repos/gambit-lego-pkg/
