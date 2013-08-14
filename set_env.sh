56db646fc0eac2a692a26c6863c7c6185907ff20
robotlab <robotlab@celilo.(none)>
2013-08-13 22:00:42 -0700

    alias updated

--- set_env.sh
+++ set_env.sh
@@ -1,7 +1,7 @@
 # use below command to set environmental variables
 # . ./set_env.sh
 
-alias set_rosmasteruri_chessbot="export ROS_MASTER_URI=http://chess-laptop.dyn.cs.washington.edu:11311;set_ros_ip"
-alias set_rosmasteruri_chessarmbox="export ROS_MASTER_URI=http://chess-armbox.dyn.cs.washington.edu:11311;set_ros_ip"
+alias set_rosmasteruri_chessbot="export ROS_MASTER_URI=http://chess-laptop.dyn.cs.washington.edu:11311;set_ros_ip;set_ros_hostname"
+alias set_rosmasteruri_chessarmbox="export ROS_MASTER_URI=http://chess-armbox.dyn.cs.washington.edu:11311;set_ros_ip;set_ros_hostname"
 
 ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/ros-repos/gambit-lego-pkg/
